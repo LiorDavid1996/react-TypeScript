f38ddc548921e3a6ad8dcccb5f94e0d581402d52
LiorDavid1996 <liordavid356@gmail.com>
2022-09-17 20:58:53 +0300

    addTable

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,19 +1,51 @@
 import {FC} from "react"
 import Iuser from '../model/userMosel'
 import {getUser} from  '../services/user.services'
+import { useEffect, useState } from "react";
  
 const returnHelloWord =(num1:number,num2:number):string=>{
     return(
         "Hello Word"
     )
 }
-getUser()
 
-export default function Home() {
+let tempUsers: Iuser[];
+function Home() {
+  const [usersArray, setUsers] = useState(tempUsers);
+  useEffect(() => {
+    getUser().then((users: Iuser[]) => {
+      setUsers(users);
+    });
+  }, [])
+
     return(
+
         <div>
-            
-        {returnHelloWord(1,2) }
+               <table>
+        <thead>
+          <tr>
+            {usersArray ? (
+              Object.keys(usersArray[0]).map((key) => <td>{key}</td>)
+            ) : (
+              <td></td>
+            )}
+          </tr>
+        </thead>
+        <tbody>
+          {usersArray?.map((user) => (
+            <tr>
+              {Object.values(user).map((value) => (
+                typeof(value) =="string"?
+                <td>{value}</td>:
+                <td>{value.first+" "+ value.last}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+       
+        
         </div>
-    )
-}
+    )}
+export default Home
+    
